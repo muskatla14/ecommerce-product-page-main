01a49c9b9fe4815d64c0d0a26fed22da35db5e2b
Marko Robovic <markorobovic02@gmail.com>
2022-12-28 14:25:17 +0100

    active states filled

--- script/app.js
+++ script/app.js
@@ -1,19 +1,73 @@
+const cartIconHeader = document.querySelector('.cart-icon-header');
+const modal = document.querySelector('.modal');
+
 const largeImage = document.querySelector('.large-img img');
 const smallImages = document.querySelectorAll('.small-img img');
 const body = document.querySelector('body');
+
+const price = document.querySelector('.price');
+const increaseBtn = document.querySelector('.increase');
+const decreaseBtn = document.querySelector('.decrease');
+const quantity = document.querySelector('.quantity');
+let current = parseInt(quantity.innerHTML);
+
+
+// Cart
+cartIconHeader.addEventListener('click', () => {
+    modal.style.display = 'flex';
+})
+
+
+  document.addEventListener('click', (e) => {
+    console.log(e.target.parentElement);
+    if(!e.target.parentElement.classList.contains('modal') && e.target != cartIconHeader) {
+        modal.style.display = 'none';
+    }
+  })
+
+
+
+// Add quantity
+increaseBtn.addEventListener('click', () => {
+    current++;
+    quantity.innerHTML = current;
+});
+
+decreaseBtn.addEventListener('click', () => {
+    current--;
+
+    if (current < 0) {
+        current = 0
+    }
+
+    quantity.innerHTML = current;
+});
+
+
+function addItemtoCart(image, price, title) {
+    
+}
 
 
 
 
 function displayLargeImage() {
     smallImages.forEach((element, index) => {
-        element.addEventListener('click', (e) => {
-            e.target.classList.toggle('border');
+        element.addEventListener('click', () => {
             largeImage.src = `images/image-product-${index + 1}.jpg`
         })
+        element.parentElement.addEventListener('focus', (event) => {
+            console.log(event.target)
+            event.target.style.borderRadius = '10px';
+            event.target.style.border = '4px solid hsl(26, 100%, 55%)';
+            // event.target.style.filter = 'opacity(0.20)'; 
+        }, true)
+        element.parentElement.addEventListener('blur', (event) => {
+            // event.target.style.filter = 'none';
+            event.target.style.border = 'none';
+        }, true)
     })
 }
-
 
 
 function displayLightboxGallery() {
@@ -48,39 +102,19 @@ function displayLightboxGallery() {
 
     // Create large image element
     const largeImageLightBox = document.createElement('img');
-    largeImageLightBox.src = 'images/image-product-1.jpg';  
+    largeImageLightBox.src = 'images/image-product-1.jpg';
     largeImageLightBox.classList.add('large-img');
 
 
     // Append large image to gallery
     lightboxGallery.appendChild(largeImageLightBox);
-    
+
 
     const thumbnailsContainer = document.createElement('div');
     thumbnailsContainer.classList.add('thumbnailsContainer');
 
     lightboxGallery.appendChild(thumbnailsContainer);
 
-    // const thumbnailDiv = document.createElement('div');
-    // thumbnailsContainer.appendChild(thumbnailDiv);
-    // const thumbnail1 = document.createElement('img');
-    // thumbnail1.src = 'images/image-product-1-thumbnail.jpg';
-    // thumbnailDiv.appendChild(thumbnail1);
-    
-    // const thumbnail2 = document.createElement('img');
-    // thumbnail2.src = 'images/image-product-2-thumbnail.jpg';
-    // lightboxGallery.appendChild(thumbnail2);
-    // const thumbnail3 = document.createElement('img');
-    // thumbnail3.src = 'images/image-product-3-thumbnail.jpg';
-    // lightboxGallery.appendChild(thumbnail3);
-    // const thumbnail4 = document.createElement('img');
-    // thumbnail4.src = 'images/image-product-4-thumbnail.jpg';
-    // lightboxGallery.appendChild(thumbnail4);
-
-    // thumbnailsContainer.appendChild(thumbnail1);
-    // thumbnailsContainer.appendChild(thumbnail2);
-    // thumbnailsContainer.appendChild(thumbnail3);
-    // thumbnailsContainer.appendChild(thumbnail4);
 
 
     // Version2
@@ -95,13 +129,30 @@ function displayLightboxGallery() {
 
 
     const thumbnailsArr = [thumbnail1, thumbnail2, thumbnail3, thumbnail4];
-    
-    thumbnailsArr.forEach(image => {
+
+    console.log(thumbnailsArr);
+
+    thumbnailsArr.forEach((element, index) => {
+        element.style.cursor = 'pointer';
         const imageDiv = document.createElement('div');
-        imageDiv.appendChild(image);
+        element.setAttribute("tabindex", "1")
+        imageDiv.appendChild(element);
         thumbnailsContainer.appendChild(imageDiv);
+        element.addEventListener('click', (e) => {
+            largeImageLightBox.src = `images/image-product-${index + 1}.jpg`
+        })
+        element.parentElement.addEventListener('focus', (event) => {
+            console.log(event.target)
+            event.target.style.borderRadius = '10px';
+            event.target.style.border = '4px solid hsl(26, 100%, 55%)';
+            // event.target.style.filter = 'opacity(0.20)'; 
+        }, true)
+        element.parentElement.addEventListener('blur', (event) => {
+            // event.target.style.filter = 'none';
+            event.target.style.border = 'none';
+        }, true)
     });
-    
+
     // Icon next and previous
     const next = document.createElement('img');
     next.src = 'images/icon-next.svg';
@@ -111,7 +162,36 @@ function displayLightboxGallery() {
     previous.classList.add('previousBtn');
     lightboxGallery.appendChild(next);
     lightboxGallery.appendChild(previous);
+
+
+
+    // Slider
+    const slideImages = ['image-product-1.jpg', 'image-product-2.jpg', 'image-product-3.jpg', 'image-product-4.jpg'];
+
+    let i = 0;
+
+    function prev() {
+        if (i <= 0) i = slideImages.length;
+        i--;
+        return setImg();
+    }
+
+    function nextImg() {
+        if (i >= slideImages.length - 1) i = -1;
+        i++;
+
+        return setImg();
+    }
+
+
+    function setImg() {
+        return largeImageLightBox.setAttribute('src', 'images/' + slideImages[i])
+    }
+
+    previous.addEventListener('click', prev);
+    next.addEventListener('click', nextImg);
 }
+
 
 displayLargeImage();
 largeImage.addEventListener('click', displayLightboxGallery)
